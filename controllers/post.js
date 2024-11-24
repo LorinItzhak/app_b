06861edca205074e5b9c69f05ba2ea67106863ff
LorinItzhak <lorinitzhak284@gmail.com>
2024-11-24 15:56:41 +0200

    עד הרצאה 2

--- controllers/post.js
+++ controllers/post.js
@@ -1,12 +1,53 @@
-const getAllPost = (req,res)=> {
-    console.log("post get All service");
-    res.send("post get All service");
+const Posts= require("../models/posts_model");
+const mongoose = require("mongoose");
+
+
+const getAllPost = async(req,res)=> {
+    const filter= req.query;
+    console.log(filter);
+    try{
+        if(filter.owner){
+            const posts = await Posts.find({owner: filter.owner});
+            return res.send(posts);
+        }
+        else{
+        const posts= await Posts.find()
+       return res.send(posts);
+        }
+    }
+    catch(err){ return res.status(400).send(err.message);}
 };
 
-const createPost = (req,res)=> {
-    console.log("post create service");
-    res.send("post create service");
+const getPostById = async (req, res) => {
+    const postId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(postId)) {
+        return res.status(400).send("Invalid post ID format");
+    }
+
+    try {
+        const post = await Posts.findById(postId);
+        if (post) {
+            return res.send(post);
+        } else {
+            return res.status(404).send("Post not found");
+        }
+    } catch (err) {
+        return res.status(400).send(err.message);
+    }
 };
+
+const createPost = async (req,res)=> {
+    console.log(req.body);
+    try{
+    const post = await Posts.create(req.body);
+    res.status(201).send(post);
+    }
+    catch(err){
+        res.status(400).send(err.message);
+    }
+  };
+
 
 const deleteAPost = (req,res)=> {
     console.log("delete a post");
@@ -20,4 +61,4 @@ const updateAPost = (req,res)=> {
 
 
 
-module.exports = { getAllPost , createPost, deleteAPost,updateAPost};
+module.exports = { getAllPost , createPost, deleteAPost,updateAPost,getPostById};
--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const post_ = require('../controllers/post');
 
 router.get('/',post_.getAllPost);
+router.get('/:id',post_.getPostById);
 router.post('/',post_.createPost);
-router.delete('/', post_.deleteAPost);
+router.delete('/:id', post_.deleteAPost);
 router.put('/', post_.updateAPost);
 
 
